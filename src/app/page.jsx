eec969e4e89a772339487131cfb6be4a35f2df75
AndreRimes <andre.2004.rimes@gmail.com>
2025-04-05 12:21:13 -0300

    fix: remove unused useEffect and correct header text

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,18 +3,13 @@ import Hero from "@/components/Hero"
 import Cards from "@/components/Cards"
 import Scrool from "@/components/scroll"
 import Info from "@/components/Info"
-import { useEffect } from "react"
 
 export default function main() {
-
-    useEffect(() => {
-        console.log("PB", process.env.NEXT_PUBLIC_PB)
-    }, [])
 
     return (
         <>
             <div className="w-full h-20 flex items-center justify-center">
-                <h1 className="font-bold text-3xl">On A Budget TESte</h1>
+                <h1 className="font-bold text-3xl">On A Budget</h1>
             </div>
             <div className="w-full flex flex-col px-5">
                 <div className="w-full h-[70vh] flex flex-row px-4">
